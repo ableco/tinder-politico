981540871a5e7e2e7cacac74bcb784b8259d9b71
cristianbgp <cristian.granda.pastor@gmail.com>
2021-01-30 14:13:50 -0500

    Add button component

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,23 @@
+/**
+ *
+ *
+ * @export
+ * @param {Object} props
+ * @param {("default" | "disabled")} props.appearance - appearance styles
+ * @param {String} label - the text that displays this button
+ * @returns
+ */
 export default function Button({ appearance = "default", label }) {
   const styles = {
     default:
       "bg-gradient-to-r from-primaryGradient-start to-primaryGradient-end text-white",
+    disabled: "bg-neutral-400 text-white",
   };
 
   return (
-    <button className={`${styles[appearance]} rounded-2xl px-8 py-2`}>
+    <button
+      className={`${styles[appearance]} font-normal rounded-2xl px-8 py-2`}
+    >
       {label}
     </button>
   );
