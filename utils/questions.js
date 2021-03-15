786c7cc5dd4b7b6986e1e434caef04f254a1bd69
cristianbgp <cristian.granda.pastor@gmail.com>
2021-03-15 08:24:06 -0500

    Update questions

--- utils/questions.js
+++ utils/questions.js
@@ -2,13 +2,14 @@ export const questions = [
   {
     id: 1,
     question:
-      "¿Cuál es su postura sobre la Constitución Política del Perú promulgada en 1993?",
+      "¿Cuál es tu postura sobre la Constitución Política del Perú de 1993?",
     answers: {
       value1: "Debe hacerse una nueva",
       value2: "Se debe someter a votación si queremos una nueva o no",
       value3: "Algunas partes deben ser modificadas",
       value4: null,
-      value5: "Debe quedarse como está",
+      value5:
+        "Más allá de mi postura, se debe someter a votación si todos los peruanos queremos una nueva o no",
     },
   },
   {
@@ -16,87 +17,93 @@ export const questions = [
     question: "¿Cuál debe ser el rol del Estado en el sector empresarial?",
     answers: {
       value1:
-        "El Estado debe fiscaliza pero también debe ser dueño de empresas",
+        "El Estado debe fiscalizar, pero también debe ser dueño de empresas",
       value2: null,
       value3: null,
       value4: null,
-      value5: "Las empresas deben ser privadas y el Estado solo fiscalizarlas",
+      value5:
+        "Las empresas deben ser privadas y el Estado solo tiene que fiscalizarlas",
     },
   },
   {
     id: 3,
-    question:
-      "¿El gobierno debería impulsar un impuesto a las grandes fortunas?",
+    question: "¿El Estado debería impulsar un impuesto a las grandes fortunas?",
     answers: {
-      value1: "A favor",
+      value1: "Si",
       value2: null,
       value3: "Me da igual",
       value4: null,
-      value5: "En contra",
+      value5: "No",
     },
   },
   {
     id: 4,
-    question: "¿El estado debe controlar los precios?",
+    question: "¿El Estado debe controlar los precios?",
     answers: {
       value1: "Sí, siempre",
       value2: null,
-      value3: "Sólo regular en el caso ciertos productos o servicios",
+      value3: "Solo para ciertos productos o servicios",
       value4: null,
       value5: "No, nunca",
     },
   },
   {
     id: 5,
-    question: "Los monopolios en el mercado peruano…",
+    question:
+      "Sobre la concentración de poder en el mercado (que solo un grupo o muy pocos dominen la oferta de un producto o servicio)...",
     answers: {
-      value1: "No deberían existir",
+      value1: "No debe haber concentración de poder en el mercado nunca",
       value2: null,
-      value3: "Sólo deben existir si el Estado los regula",
+      value3: "Solo debe haber si el Estado lo regula",
       value4: null,
-      value5: "No hay problema con que existan",
+      value5:
+        "No hay problema con que haya concentración de poder en el mercado",
     },
   },
   {
     id: 6,
-    question: "Las escuelas o universidades privadas…",
+    question:
+      "Las escuelas y universidades privadas que se manejan como empresas...",
     answers: {
       value1:
-        "Deben cumplir con un estándar mínimo de calidad regulado por el estado y pagar impuestos como cualquier empresa privada",
+        "Deben cumplir, por lo menos, con un estándar mínimo de calidad regulado por el Estado y pagar impuestos como cualquier empresa privada",
       value2:
-        "Deben cumplir con un estándar mínimo de calidad regulado por el estado y no pagar impuestos por el servicio que brindan",
+        "Deben cumplir, por lo menos, con un estándar mínimo de calidad regulado por el Estado y no pagar impuestos",
       value3: null,
-      value4:
-        "Deben ser libres de gestionarse y pagar impuestos como cualquier empresa privada",
-      value5:
-        "Deben ser libres de gestionarse y no pagar impuestos por el servicio que brindan",
+      value4: "No deben ser fiscalizadas, pero deben pagar impuestos",
+      value5: "No deben ser fiscalizadas ni pagar impuestos",
     },
   },
   {
     id: 7,
-    question: "¿Quién crees que deba hacer el proceso de vacunación?",
+    question: "El proceso de vacunación…",
     answers: {
-      value1: "Solo el Estado siempre",
-      value2: "Solo el Estado y quizá después el privado",
+      value1: "Debe estar a cargo del Estado de inicio a fin",
+      value2:
+        "Debe estar a cargo del Estado por ahora y después podrían intervenir los privados",
       value3: null,
-      value4: "El Estado con ayuda del privado pero regulado para que no lucre",
-      value5: "El Estado y el privado como mejor le parezca",
+      value4:
+        "Debe estar a cargo del Estado con ayuda de los privados, pero con regulaciones",
+      value5:
+        "Debe realizarse tanto desde el Estado como desde el sector privado sin restricciones",
     },
   },
   {
     id: 8,
-    question: "Sobre el aborto, yo considero que…",
+    question:
+      "Sobre la legalización del aborto, teniendo en cuenta que ya es legal si la vida de la madre está en peligro, consideras que...",
     answers: {
-      value1: "Debería ser legal en todos los casos",
-      value2: "Debería ser legal solo en algunos casos",
+      value1: "Debería ser legal en todos los demás casos",
+      value2: "Debería ser legal solo en algunos casos más",
       value3: "No tengo posición",
       value4: null,
-      value5: "No debería ser legal en ningún caso",
+      value5: "No debería ser legal en ningún caso más",
     },
   },
   {
     id: 9,
-    question: "Apoya la unión civil entre personas del mismo sexo?",
+    question:
+      "¿Qué opinas sobre la legalización de la unión civil entre personas del mismo sexo?",
     answers: {
       value1: null,
       value2: "A favor",
@@ -108,9 +115,9 @@ export const questions = [
   {
     id: 10,
     question:
-      "¿Cuál es su postura frente a la deportación de extranjeros ilegales?",
+      "¿Cuál es tu postura frente a la deportación de extranjeros ilegales?",
     answers: {
-      value1: "En contra, se debe ayudar no deportar",
+      value1: "En contra, se debe ayudar, no deportar",
       value2: null,
       value3: "Me da igual",
       value4: null,
@@ -119,19 +126,19 @@ export const questions = [
   },
   {
     id: 11,
-    question: "Sobre la eutanasia, yo considero que…",
+    question: "Sobre la eutanasia, considero que...",
     answers: {
-      value1: "A favor",
+      value1: "Debería estar permitida",
       value2: null,
       value3: "No tengo posición",
       value4: null,
-      value5: "En contra",
+      value5: "No debería estar permitida",
     },
   },
   {
     id: 12,
     question:
-      "La Educación Sexual Integral (ESI) busca brindar información oportuna a niños, niñas y adolescentes para su protección y toma de decisiones en su autocuidado respecto al tema. ¿Qué opinas sobre ella?",
+      "La Educación Sexual Integral (ESI) busca brindar información oportuna a niños, niñas y adolescentes para su protección y toma de decisiones en su autocuidado respecto al tema. ¿Qué opinas de ella?",
     answers: {
       value1: "Debe realizarse tanto en casa como en las escuelas.",
       value2: null,
@@ -144,15 +151,15 @@ export const questions = [
   {
     id: 13,
     question:
-      "¿Cómo cree que debería financiarse el sistema de pensiones para la vejez?",
+      "¿Cómo crees que debería financiarse el sistema de pensiones para la vejez?",
     answers: {
       value1:
-        "El estado debería financiar pensiones mínimas para todos a través de impuestos principalmente.",
+        "El estado debería financiar las pensiones para todos a través de impuestos principalmente",
       value2:
-        "Cada trabajador ahorraría para su vejez en un sistema de fondo individual, y el estado financia con impuestos pensiones mínimas a quienes necesiten (sistema mixto 1)",
+        "Cada trabajador ahorraría para su vejez en un sistema de fondo individual, y el estado financia con impuestos las pensiones de quienes necesiten",
       value3: null,
       value4:
-        "Cada trabajador ahorraría para su vejez en un sistema de fondo individual, aportando una parte a un fondo solidario para garantizar pensiones mínimas a quienes necesiten (sistema mixto 2)",
+        "Cada trabajador ahorraría para su vejez en un sistema de fondo individual, aportando una parte a un fondo solidario para garantizar las pensiones de quienes necesiten",
       value5: null,
     },
   },
@@ -162,27 +169,26 @@ export const questions = [
       "¿Qué opinas sobre el licenciamiento de universidades y de institutos?",
     answers: {
       value1:
-        "Licenciamiento sujeto a renovación por parte de la SUNEDU/MINEDU.",
+        "Se les debe dar una licencia que se renueve cada cierto tiempo por parte de la SUNEDU/MINEDU",
       value2: null,
       value3:
-        "Un solo licenciamiento válido para siempre por parte de la SUNEDU/MINEDU.",
+        "Se les debe dar una sola licencia válida para siempre por parte de la SUNEDU/MINEDU",
       value4: null,
       value5:
-        "No debería haber licenciamiento o lo deberían hacer terceros privados.",
+        "No deberían necesitar licenciamiento o lo deberían hacer terceros privados",
     },
   },
   {
     id: 15,
     question:
-      "¿Cómo solucionamos el acceso a internet por parte de los estudiantes?",
+      "¿Cuál sería tu principal apuesta para que más estudiantes tengan acceso a internet?",
     answers: {
       value1:
-        "Aumento del presupuesto al sector educación destinado a cierre de brechas digitales",
-      value2:
-        "Masificación del internet satelital mediante la compra de satelite y/o uso de mecanismos ya existentes",
+        "Solo aumentando el presupuesto del sector educación que esta destinado a cierre de brechas digitales",
+      value2: "Comprar un satélite",
       value3: null,
       value4:
-        "Ampliación de la fibra óptica, de nuevas redes y/o de las ya existentes",
+        "Activar e invertir en la red dorsal nacional de fibra óptica que ya existe",
       value5: null,
     },
   },
