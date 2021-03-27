ad5ba5abdf3a2c6b128b0ffece8084e71d2e307f
cristianbgp <cristian.granda.pastor@gmail.com>
2021-03-27 16:48:01 -0500

    Add ver resultados link

--- components/RestCandidateCard.js
+++ components/RestCandidateCard.js
@@ -6,40 +6,41 @@ export default function RestCandidateCard({ candidate }) {
     <div className="mb-5 w-full">
       <div className="flex px-1 w-full justify-between">
         <div className="flex items-center">
-          <Link
-            href={`https://www.yovoto.pe/candidates/${candidate.data.id}?activeTab=Perfil+Político`}
-          >
-            <a rel="nofollow noopener noreferrer external" target="_blank">
-              <img
-                className="w-12 h-12 bg-white rounded-full object-cover border-white border-2 shadow-button grayscale"
-                src={candidate.data.profile_photo_url}
-                alt={titleize(
-                  `${candidate.data.names}  ${candidate.data.family_name}`,
-                )}
-              />
-            </a>
-          </Link>
-          <img
-            className="w-7 h-7 bg-white rounded-sm object-cover ml-2 border-white border-2 shadow-button"
-            src={candidate.data.political_organization.logo}
-            alt={titleize(candidate.data.political_organization.name)}
-          />
+          <div className="flex items-center justify-center flex-col xs:flex-col sm:flex-row md:flex-row">
+            <img
+              className="w-12 h-12 bg-white rounded-full object-cover border-white border-2 shadow-button grayscale"
+              src={candidate.data.profile_photo_url}
+              alt={titleize(
+                `${candidate.data.names}  ${candidate.data.family_name}`,
+              )}
+            />
+            <img
+              className="w-7 h-7 bg-white rounded-sm object-cover border-white border-2 shadow-button ml-0 sm:ml-2 md:ml-2 mt-2 sm:mt-0 md:mt-0"
+              src={candidate.data.political_organization.logo}
+              alt={titleize(candidate.data.political_organization.name)}
+            />
+          </div>
           <div className="mx-3 flex flex-col justify-start">
             <div>
-              <Link
-                href={`https://www.yovoto.pe/candidates/${candidate.data.id}?activeTab=Perfil+Político`}
-              >
-                <a rel="nofollow noopener noreferrer external" target="_blank">
-                  <p className="font-extrabold text-xl text-black">
-                    {titleize(
-                      `${candidate.data.names}  ${candidate.data.family_name}`,
-                    )}
-                  </p>
-                </a>
-              </Link>
+              <p className="font-extrabold text-xl text-black">
+                {titleize(
+                  `${candidate.data.names}  ${candidate.data.family_name}`,
+                )}
+              </p>
               <p className="font-extrabold text-xs text-gray-base uppercase">
                 {titleize(candidate.data.political_organization.name)}
               </p>
+              <Link
+                href={`https://www.yovoto.pe/candidates/${candidate.data.id}?activeTab=Perfil+Político`}
+              >
+                <a
+                  rel="nofollow noopener noreferrer external"
+                  target="_blank"
+                  className="font-extrabold text-black text-center underline mt-1 mb-1"
+                >
+                  Ver resultados
+                </a>
+              </Link>
             </div>
           </div>
         </div>
