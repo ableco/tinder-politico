53495de9aeb38b43695fe4fa256ee9528de3cb62
hpneo <hpneo@hotmail.com>
2021-04-01 10:54:13 -0500

    Update answers

--- utils/calculateResults.js
+++ utils/calculateResults.js
@@ -2,7 +2,7 @@ import calculateAveragePercentage from "./calculateAveragePercentage";
 import { candidates as allCandidatesData } from "../data/candidates.json";
 
 // Yonhy Lescano Ancieta
-const temporalResultsCandidate1 = {
+const candidateAnswers1 = {
   id: 42,
   answers: {
     1: 1,
@@ -21,54 +21,57 @@ const temporalResultsCandidate1 = {
     14: 1,
     15: 2,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/accion-popular/",
 };
 
 // Cesar Acuña Peralta
-const temporalResultsCandidate2 = {
+const candidateAnswers2 = {
   id: 130,
   answers: {
     1: 2,
     2: 5,
-    3: 5,
+    3: 3,
     4: 5,
     5: 2,
-    6: 2,
+    6: 1,
     7: 5,
     8: 3,
     9: 3,
     10: 3,
-    11: 5,
+    11: 3,
     12: 3,
     13: 2,
     14: 2,
     15: 2,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/alianza-para-el-progreso/",
 };
 
 // Hernando De Soto Polar
-const temporalResultsCandidate3 = {
+const candidateAnswers3 = {
   id: 108,
   answers: {
     1: 2,
     2: 5,
     3: 5,
     4: 5,
-    5: 3,
+    5: 1,
     6: 2,
     7: 5,
-    8: 5,
+    8: 3,
     9: 5,
-    10: 3,
+    10: 5,
     11: 3,
     12: 3,
     13: 2,
     14: 3,
     15: 2,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/avanza-pais/",
 };
 
 // Andres Avelino Alcantara Paredes
-const temporalResultsCandidate4 = {
+const candidateAnswers4 = {
   id: 86,
   answers: {
     1: 1,
@@ -87,10 +90,11 @@ const temporalResultsCandidate4 = {
     14: 3,
     15: 2,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/democracia-directa/",
 };
 
 // Marco Antonio Arana Zegarra
-const temporalResultsCandidate5 = {
+const candidateAnswers5 = {
   id: 159,
   answers: {
     1: 1,
@@ -99,8 +103,8 @@ const temporalResultsCandidate5 = {
     4: 2,
     5: 1,
     6: 1,
-    7: 1,
-    8: 2,
+    7: 2,
+    8: 1,
     9: 1,
     10: 3,
     11: 3,
@@ -109,10 +113,11 @@ const temporalResultsCandidate5 = {
     14: 1,
     15: 4,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/frente-amplio/",
 };
 
 // Keiko Sofia Fujimori Higuchi
-const temporalResultsCandidate6 = {
+const candidateAnswers6 = {
   id: 75,
   answers: {
     1: 5,
@@ -131,10 +136,11 @@ const temporalResultsCandidate6 = {
     14: 1,
     15: 4,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/fuerza-popular/",
 };
 
 // Veronika Fanny Mendoza Frisch
-const temporalResultsCandidate7 = {
+const candidateAnswers7 = {
   id: 65,
   answers: {
     1: 1,
@@ -153,10 +159,11 @@ const temporalResultsCandidate7 = {
     14: 1,
     15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/juntos-por-el-peru/",
 };
 
 // Daniel Enrique Salaverry Villa
-const temporalResultsCandidate8 = {
+const candidateAnswers8 = {
   id: 31,
   answers: {
     1: 1,
@@ -175,10 +182,11 @@ const temporalResultsCandidate8 = {
     14: 1,
     15: 4,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/somos-peru/",
 };
 
 // Julio Armando Guzman Caceres
-const temporalResultsCandidate9 = {
+const candidateAnswers9 = {
   id: 9,
   answers: {
     1: 3,
@@ -195,34 +203,36 @@ const temporalResultsCandidate9 = {
     12: 1,
     13: 3,
     14: 1,
-    15: 4,
+    15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/partido-morado/",
 };
 
 // Ollanta Moises Humala Tasso
-const temporalResultsCandidate10 = {
+const candidateAnswers10 = {
   id: 97,
   answers: {
     1: 3,
     2: 1,
     3: 1,
     4: 2,
-    5: 5,
+    5: 1,
     6: 2,
     7: 2,
     8: 1,
     9: 1,
     10: 1,
-    11: 1,
+    11: 3,
     12: 1,
     13: 2,
     14: 1,
     15: 4,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/partido-nacionalista/",
 };
 
 // Jose Pedro Castillo Terrones
-const temporalResultsCandidate11 = {
+const candidateAnswers11 = {
   id: 168,
   answers: {
     1: 1,
@@ -231,20 +241,21 @@ const temporalResultsCandidate11 = {
     4: 2,
     5: 1,
     6: 1,
-    7: 3,
+    7: 1,
     8: 2,
     9: 3,
-    10: 5,
+    10: 1,
     11: 3,
     12: 3,
     13: 1,
     14: 3,
     15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/peru-libre/",
 };
 
 // Alberto Ismael Beingolea Delgado
-const temporalResultsCandidate12 = {
+const candidateAnswers12 = {
   id: 206,
   answers: {
     1: 5,
@@ -253,25 +264,26 @@ const temporalResultsCandidate12 = {
     4: 5,
     5: 2,
     6: 1,
-    7: 5,
-    8: 2,
-    9: 5,
+    7: 2,
+    8: 5,
+    9: 1,
     10: 5,
-    11: 5,
+    11: 3,
     12: 5,
     13: 2,
     14: 1,
     15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/partido-popular-cristiano/",
 };
 
 // Rafael Gaston Tadeo Milagros Santos Normand
-const temporalResultsCandidate13 = {
+const candidateAnswers13 = {
   id: 189,
   answers: {
-    1: 5,
+    1: 2,
     2: 5,
-    3: 5,
+    3: 3,
     4: 2,
     5: 1,
     6: 2,
@@ -279,16 +291,17 @@ const temporalResultsCandidate13 = {
     8: 5,
     9: 3,
     10: 5,
-    11: 3,
+    11: 1,
     12: 3,
     13: 2,
     14: 3,
     15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/peru-patria-segura/",
 };
 
 // Daniel Belizario Urresti Elera
-const temporalResultsCandidate14 = {
+const candidateAnswers14 = {
   id: 119,
   answers: {
     1: 2,
@@ -298,7 +311,7 @@ const temporalResultsCandidate14 = {
     5: 1,
     6: 2,
     7: 5,
-    8: 5,
+    8: 2,
     9: 1,
     10: 5,
     11: 1,
@@ -307,10 +320,11 @@ const temporalResultsCandidate14 = {
     14: 1,
     15: 4,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/podemos-peru/",
 };
 
 // Ciro Alfredo Galvez Herrera
-const temporalResultsCandidate15 = {
+const candidateAnswers15 = {
   id: 53,
   answers: {
     1: 1,
@@ -321,7 +335,7 @@ const temporalResultsCandidate15 = {
     6: 4,
     7: 4,
     8: 2,
-    9: 5,
+    9: 1,
     10: 3,
     11: 1,
     12: 3,
@@ -329,10 +343,11 @@ const temporalResultsCandidate15 = {
     14: 3,
     15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/runa/",
 };
 
 // George Patrick Forsyth Sommer
-const temporalResultsCandidate16 = {
+const candidateAnswers16 = {
   id: 149,
   answers: {
     1: 2,
@@ -351,16 +366,17 @@ const temporalResultsCandidate16 = {
     14: 1,
     15: 2,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/victoria-nacional/",
 };
 
 // Rafael Bernardo Lopez Aliaga Cazorla
-const temporalResultsCandidate17 = {
+const candidateAnswers17 = {
   id: 198,
   answers: {
     1: 2,
     2: 5,
     3: 5,
-    4: 3,
+    4: 2,
     5: 3,
     6: 4,
     7: 5,
@@ -371,57 +387,59 @@ const temporalResultsCandidate17 = {
     12: 5,
     13: 2,
     14: 5,
-    15: 2,
+    15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/renovacion-popular/",
 };
 
 // Jose Alejandro Vega Antonio
-const temporalResultsCandidate18 = {
+const candidateAnswers18 = {
   id: 203,
   answers: {
     1: 1,
     2: 1,
     3: 1,
-    4: 1,
+    4: 3,
     5: 1,
-    6: 2,
+    6: 1,
     7: 1,
-    8: 3,
-    9: 5,
-    10: 3,
+    8: 5,
+    9: 3,
+    10: 5,
     11: 3,
     12: 3,
     13: 1,
     14: 5,
     15: 1,
   },
+  source: "https://impulsopais.pe/p/yovoto/fuentes/union-por-el-peru/",
 };
 
-const temporalCandidateAnswers = [
-  temporalResultsCandidate1,
-  temporalResultsCandidate2,
-  temporalResultsCandidate3,
-  temporalResultsCandidate4,
-  temporalResultsCandidate5,
-  temporalResultsCandidate6,
-  temporalResultsCandidate7,
-  temporalResultsCandidate8,
-  temporalResultsCandidate9,
-  temporalResultsCandidate10,
-  temporalResultsCandidate11,
-  temporalResultsCandidate12,
-  temporalResultsCandidate13,
-  temporalResultsCandidate14,
-  temporalResultsCandidate15,
-  temporalResultsCandidate16,
-  temporalResultsCandidate17,
-  temporalResultsCandidate18,
+const candidateAnswers = [
+  candidateAnswers1,
+  candidateAnswers2,
+  candidateAnswers3,
+  candidateAnswers4,
+  candidateAnswers5,
+  candidateAnswers6,
+  candidateAnswers7,
+  candidateAnswers8,
+  candidateAnswers9,
+  candidateAnswers10,
+  candidateAnswers11,
+  candidateAnswers12,
+  candidateAnswers13,
+  candidateAnswers14,
+  candidateAnswers15,
+  candidateAnswers16,
+  candidateAnswers17,
+  candidateAnswers18,
 ];
 
 export default function calculateResults(results) {
   try {
     let candidates = Object.entries(
-      temporalCandidateAnswers
+      candidateAnswers
         .map((candidate) => ({
           id: candidate.id,
           percentage: calculateAveragePercentage(
